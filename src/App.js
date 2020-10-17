362cb904ca1b8e7f078457de8b5924d7e5ef5a7f
Yuhan <yuhanx@andrew.cmu.edu>
2020-10-16 20:36:30 -0700

    init w/ create react app, router dom, antd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { FC } from 'react';
+import React from 'react';
 import { Button } from 'antd';
 import './App.css';
 
-const App: FC = () => (
+const App = () => (
   <div className="App">
     <Button type="primary">Button</Button>
   </div>
